80db53ea1ac3f97448a8fbcac3a2f77cb640fe83
KimMinheee <99alsgmlalsl@naver.com>
2022-10-30 16:26:36 +0900

    [Feat] : q3_문장 속 단어 solution2 추가

--- algorithm_lecture/section1/q3_문장속단어/Main.java
+++ algorithm_lecture/section1/q3_문장속단어/Main.java
@@ -1,6 +1,5 @@
 package algorithm_lecture.section1.q3_문장속단어;
-
-        import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
     public static String solution(String data){
@@ -13,14 +12,34 @@ public class Main {
                 count = strList[i].length();
             }
         }
-
         return strList[index];
+    }
+
+    public static String solution2(String data){
+        String answer = "";
+        int m = Integer.MIN_VALUE, pos;
+        while((pos = data.indexOf(' ')) != -1){
+            String tmp = data.substring(0,pos); //0번째인덱스 ~ (pos-1)번째 인덱스까지 자르기
+
+            int len = tmp.length();
+            if(len>m){
+                m = len;
+                answer = tmp;
+            }
+            data = data.substring(pos+1); //문자열 다시 갱신
+        }
+        if(data.length()>m){
+            answer = data;
+        }
+        return answer;
     }
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         String data = scan.nextLine();
 
-        System.out.println(solution(data));
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println(solution2(data));
     }
 }
