febfe2eeead072d41f71fa8765972a4ca9ef38c5
KimMinheee <99alsgmlalsl@naver.com>
2022-10-30 21:50:59 +0900

    [Feat] : q5_특정 문자 뒤집기 solution 추가 - if-else문 줄임

--- algorithm_lecture/section1/q5_특정문자뒤집기/Main.java
+++ algorithm_lecture/section1/q5_특정문자뒤집기/Main.java
@@ -33,9 +33,29 @@ public class Main {
         }
         return String.valueOf(wordList); //
     }
+    public static String solution2(String word){
+        char[] wordList = word.toCharArray();
+        int fp = 0; //frontPointer
+        int bp = wordList.length-1; //backPointer
+
+        while(fp<bp){
+            if(!Character.isAlphabetic(wordList[fp])) fp++;
+            else if(!Character.isAlphabetic(wordList[bp])) bp--;
+            else{
+                char tmp = wordList[fp];
+                wordList[fp] = wordList[bp];
+                wordList[bp] = tmp;
+                fp++;
+                bp--;
+            }
+        }
+        return String.valueOf(wordList);
+
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
-        System.out.println(solution(word));
+        System.out.println(solution2(word));
     }
 }
