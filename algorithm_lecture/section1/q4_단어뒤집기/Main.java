5d0976ce375391b8695eb4a1c4a3429f27819d14
KimMinheee <99alsgmlalsl@naver.com>
2022-10-30 18:08:28 +0900

    [Feat] : q4_단어 뒤집기 solution 추가 - StringBuilder() 클래스의 reverse()메소드 활용 - for&while문으로 solution 개선

--- algorithm_lecture/section1/q4_단어뒤집기/Main.java
+++ algorithm_lecture/section1/q4_단어뒤집기/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 //todo : 실행시간 체크
 
 public class Main {
+
+    //for문 중첩으로 시간복잡도 best인 풀이X
     public static String[] solution(String[] words){
         String data = "";
         
@@ -16,6 +18,7 @@ public class Main {
         }
         return words;
     }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int cnt = Integer.parseInt(sc.next());
