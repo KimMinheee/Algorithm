d61f14a77e9a2047994414fce258e0538856f72c
KimMinheee <99alsgmlalsl@naver.com>
2022-11-30 23:56:12 +0900

    [Feat] : q5_소수(에라토스테네스 체) 배열 크기 변경

--- algorithm_lecture/ch2_Array/q5_소수에라토스테네스체/Main.java
+++ algorithm_lecture/ch2_Array/q5_소수에라토스테네스체/Main.java
@@ -3,13 +3,14 @@ package algorithm_lecture.ch2_Array.q5_소수에라토스테네스체;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static int solution(int num){
         int cnt = 0; // 소수의 개수
 
-        //배열 선언 - 0으로 초기화 {0,0,0,...,0}
-        int arr[] = new int[num];
+        //배열 선언 - 0으로 초기화 {0,0,0,...,0},i는 0부터 들어감. 0~n 까지의 자연수 이므로 n+1
+        int arr[] = new int[num+1];
 
         //소수 여부 파악
         for(int i=2; i<num;i++){
