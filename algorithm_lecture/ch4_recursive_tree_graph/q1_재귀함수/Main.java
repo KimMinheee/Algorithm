b369ba8e68770e173f0827ec62f04ff89fab36eb
KimMinheee <99alsgmlalsl@naver.com>
2022-12-25 23:40:48 +0900

    [Feat] : q1_재귀함수(스택프레임) solved.

---
+++ algorithm_lecture/ch4_recursive_tree_graph/q1_재귀함수/Main.java
@@ -0,0 +1,21 @@
+package algorithm_lecture.ch4_recursive_tree_graph.q1_재귀함수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void solution(int num){
+        if(num==0) return;
+        System.out.print(num+" ");
+        solution(num-1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+
+        solution(num);
+    }
+}
