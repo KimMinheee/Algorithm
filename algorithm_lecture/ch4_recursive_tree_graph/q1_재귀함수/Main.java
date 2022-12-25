c4becd05f22069a433fa1ddd00d2d6cfdd84c2f0
KimMinheee <99alsgmlalsl@naver.com>
2022-12-26 00:01:54 +0900

    [Feat] : q1_재귀함수(스택프레임) 예제 순서로 출력하도록 코드 변경

--- algorithm_lecture/ch4_recursive_tree_graph/q1_재귀함수/Main.java
+++ algorithm_lecture/ch4_recursive_tree_graph/q1_재귀함수/Main.java
@@ -8,8 +8,9 @@ import java.io.InputStreamReader;
 public class Main {
     public static void solution(int num){
         if(num==0) return;
-        System.out.print(num+" ");
         solution(num-1);
+        System.out.print(num+" ");
+
     }
 
     public static void main(String[] args) throws IOException {
