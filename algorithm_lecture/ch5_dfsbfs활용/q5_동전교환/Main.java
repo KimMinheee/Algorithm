7af05197b34b200d711b4503fd053d929df5a4a7
KimMinheee <99alsgmlalsl@naver.com>
2023-01-09 12:03:11 +0900

    [Feat] : q5_동전교환 DFSver solved.

--- algorithm_lecture/ch5_dfsbfs활용/q5_동전교환/Main.java
+++ algorithm_lecture/ch5_dfsbfs활용/q5_동전교환/Main.java
@@ -3,9 +3,7 @@ package algorithm_lecture.ch5_dfsbfs활용.q5_동전교환;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Node{
     int depth;
@@ -18,7 +16,9 @@ class Node{
 public class Main{
     static int n; //동전의 개수
     static int total; //거슬러 줄 금액
-    static int[] data; //동전의 종류
+    static Integer[] data; //동전의 종류
+    static int answer = Integer.MAX_VALUE;
+
     static int bfs(){
         Queue que = new LinkedList<>();
         que.offer(new Node(0,0));
@@ -34,17 +34,31 @@ public class Main{
         return 0;
     }
 
+    static void dfs(int L, int sum){
+        if(sum>total) return;
+        if(L>=answer) return;
+        if(sum==total){
+            answer = Math.min(answer,L);
+        }
+        for(int i=0; i<n; i++){
+            dfs(L+1, sum+data[i]);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        data = new int[n]; //동전의 종류 배열
+        data = new Integer[n]; //동전의 종류 배열
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<n; i++){
             data[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(data, Collections.reverseOrder()); //내림차순으로 정렬.->큰 수 부터 찾도록.
         total = Integer.parseInt(br.readLine());
 
-        System.out.println(bfs());
+        //System.out.println(bfs());
+        dfs(0,0);
+        System.out.println(answer);
     }
 }
