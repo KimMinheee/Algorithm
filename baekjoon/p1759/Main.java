893007b99f1a70bf7166bb94464bf8b33946c4f8
KimMinheee <99alsgmlalsl@naver.com>
2023-02-04 01:14:27 +0900

    [Chore] : Boj p1759 주석 추가 - 로직 주석 추가 - depth 값 직관적이게 1로 수정

--- baekjoon/p1759/Main.java
+++ baekjoon/p1759/Main.java
@@ -4,6 +4,19 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
+//L(암호의 자리수), C(문자의 개수)를 입력받는다.
+//사전식으로 출력해야하고 하나씩 뽑아서 끝까지 탐색 -> 정렬 & dfs 사용
+
+//dfs로 갖고다녀야 하는 값 : 인덱스 , depth, 자음개수, 모음개수
+//dfs 순서
+//1. 체크인
+//2. 목적지에 도착했는가 : depth 확인 -> (일단 4개 채움) -> 그 4자리 수가 최소자음,모음 개수를 만족하는지 확인(print)
+//3. 연결된 곳 순회 : 배열의 현재 인덱스 다음부터 하나씩
+//4. 갈 수 있는 가 : visit 확인
+//5. 간다
+//6. 체크아웃
+
+
 public class Main {
     static int L,C;
     static char[] input;
@@ -28,10 +41,10 @@ public class Main {
         for(int i=0; i<C;i++){
             char tmp = input[i];
             if (tmp=='a'||tmp=='e'||tmp=='i'||tmp=='o'||tmp=='u') { //자음
-                dfs(i,0,0,1);
+                dfs(i,1,0,1);
             }
             else{//모음
-                dfs(i,0,1,0);
+                dfs(i,1,1,0);
             }
         }
     }
@@ -41,7 +54,7 @@ public class Main {
         visit[index] = true;
 
         //2. 목적지인가
-        if(depth == L-1){
+        if(depth == L){
             print(vowels, consonants);
         }
         //3. 연결된 곳을 순회
@@ -68,9 +81,6 @@ public class Main {
                 }
             }
             System.out.println();
-        }
-        else{
-            return;
         }
     }
 }
