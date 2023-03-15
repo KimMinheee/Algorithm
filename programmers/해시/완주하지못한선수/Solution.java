6e7eba1cbd85ad74eed53647b235861b8f565ca5
KimMinheee <99alsgmlalsl@naver.com>
2023-03-15 15:24:44 +0900

    [Chore] : 프로그래머스 완주하지 못한 선수 클래스 명 변경 - Main -> Solution

---
+++ programmers/해시/완주하지못한선수/Solution.java
@@ -0,0 +1,34 @@
+package programmers.해시.완주하지못한선수;
+
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        boolean[] check = new boolean[200001];
+        int answer = 0;
+
+        //hashMap에 데이터 세팅
+        HashMap<Integer,Integer> hashMap = new HashMap<>(200001);
+
+        for(int n : nums){
+            if(hashMap.containsKey(n)){
+                hashMap.put(n,hashMap.get(n)+1);
+            }
+            else hashMap.put(n,1);
+        }
+
+        //nums.size/2 만큼 가져가는 경우가 가장 베스트.
+        for(Integer key : hashMap.keySet()){
+            if(answer >= nums.length/2) return answer;
+
+            if(hashMap.get(key)>=1 && check[key]==false){
+                answer++;
+                check[key] = true;
+                hashMap.put(key,hashMap.get(key)-1);
+            }
+        }
+
+        return answer;
+    }
+}
