5e31b4ed1576ff981a30f6511785dfc09fa8e34f
KimMinheee <99alsgmlalsl@naver.com>
2023-03-20 23:57:39 +0900

    [문자열] : SWEA p2058_자릿수더하기 solved.

---
+++ swea/p2058_자릿수더하기/Solution.java
@@ -0,0 +1,20 @@
+package swea.p2058_자릿수더하기;
+
+import java.util.*;
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+
+        while(num != 0){
+            sum += num%10;
+            num = num/10;
+        }
+        System.out.println(sum);
+
+    }
+}
