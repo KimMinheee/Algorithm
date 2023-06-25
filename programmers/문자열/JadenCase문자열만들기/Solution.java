bfb518c2c5a0bf0859ad9fd239a5587a680f844c
KimMinheee <99alsgmlalsl@naver.com>
2023-06-26 03:07:12 +0900

    [문자열] : 프로그래머스 JadenCase 문자열 만들기 solved - 기존 에러 수정(chunk 앞,뒤 공백이 길게 있는 경우)

--- programmers/문자열/JadenCase문자열만들기/Solution.java
+++ programmers/문자열/JadenCase문자열만들기/Solution.java
@@ -1,21 +1,31 @@
 package programmers.문자열.JadenCase문자열만들기;
 
+
 class Solution {
+
     public String solution(String s) {
         StringBuilder sb = new StringBuilder();
 
-        String[] words = s.split(" ");
-        for(String word : words){
-            if(!Character.isLetter(word.charAt(0))){
-                String tmp = word.substring(1,word.length());
-                sb.append(word.charAt(0)).append(tmp.toLowerCase()).append(" ");
+        boolean isFindLetterOrDigit = false;
+        for (int i = 0; i < s.length(); i++) {
+            //앞에서부터 돌면서 공백은 추가, 맨 처음 문자/숫자는 추가
+            Character c = s.charAt(i);
+
+            if (c == ' ') {
+                sb.append(c);
+                isFindLetterOrDigit = false;
             }
-            else{
-                String tmp = word.substring(1,word.length());
-                sb.append(Character.toUpperCase(word.charAt(0))).append(tmp.toLowerCase()).append(" ");
+            //맨 처음 문자 or 숫자
+            else if (Character.isLetterOrDigit(c) && !isFindLetterOrDigit) {
+                isFindLetterOrDigit = true;
+                if (Character.isLetter(c)) sb.append(Character.toUpperCase(c));
+                if (Character.isDigit(c)) sb.append(c);
+            } else if (Character.isLetter(c) && isFindLetterOrDigit) {
+                sb.append(Character.toLowerCase(c));
             }
         }
 
+        System.out.println(sb.toString());
         return sb.toString();
     }
 }
